d06507e205d1d952810c4474bcd220acc8c0aabd
hoang phuc <hoangphucbmt12@gmail.com>
2022-08-09 23:38:31 +0700

    addRole1

---
+++ AssignFPTBook/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AssignFPTBook.Data;
+using AssignFPTBook.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AssignFPTBook.Areas.Identity.IdentityHostingStartup))]
+namespace AssignFPTBook.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AssignFPTBookContext>(options =>
+                    options.UseSqlite(
+                        context.Configuration.GetConnectionString("AssignFPTBookContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AssignFPTBookContext>();
+            });
+        }
+    }
+}
---
+++ AssignFPTBook/Utils/Role.cs
@@ -0,0 +1,10 @@
+﻿namespace AssignFPTBook.Utils
+{
+    public class Role
+    {
+        public const string ADMIN = "admin";
+        public const string USER = "user";
+        public const string STORE = "store";
+
+    }
+}
