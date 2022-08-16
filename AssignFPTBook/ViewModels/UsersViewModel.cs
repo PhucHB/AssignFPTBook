ec7fe1b56067ba9953e4d94c1c281044fa37a1f9
Tài Lê Doãn <ledoantai2204@gmail.com>
2022-08-16 13:31:48 +0700

    +Users manager screen

---
+++ AssignFPTBook/ViewModels/UsersViewModel.cs
@@ -0,0 +1,14 @@
+﻿using AssignFPTBook.Models;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AssignFPTBook.ViewModels
+{
+    public class UsersViewModel
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+    }
+}
