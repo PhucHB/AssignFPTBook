7c1ae227649952b78528f69910df706468fefaa3
Tài Lê Doãn <ledoantai2204@gmail.com>
2022-08-16 13:28:06 +0700

    + Home page for Admin

---
+++ AssignFPTBook/Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using AssignFPTBook.Data;
+using AssignFPTBook.Models;
+using AssignFPTBook.Utils;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssignFPTBook.Controllers
+{
+    [Authorize(Roles = Role.ADMIN)]
+    public class AdminController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        private readonly ApplicationDbContext _context;
+
+        public AdminController(
+          UserManager<ApplicationUser> userManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Users()
+        {
+            var usersWithPermission = _userManager.GetUsersInRoleAsync(Role.USER).Result;
+            return View(usersWithPermission);
+        }
+    }
+}
