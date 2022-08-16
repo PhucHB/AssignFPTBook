f48441e16a85f8ab882dcb716abf7383028bc1d4
hoang phuc <hoangphucbmt12@gmail.com>
2022-08-16 15:31:56 +0700

    addContactsView

--- AssignFPTBook/Models/ApplicationUser.cs
+++ AssignFPTBook/Models/ApplicationUser.cs
@@ -8,5 +8,6 @@ namespace AssignFPTBook.Models
         public string FullName { get; set; }
         public string Address { get; set; }
         public List<Book> Books { get; set; }
+        public List<ContactAdmin> contactAdmins { get; set; }
     }
 }
